92e1e9be31ec74ca2e98d753e1d9812d6a635cec
LeoGonzaga <sacredlotus05@gmail.com>
2025-06-12 21:43:07 -0300

    teste msg

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ const App = () => {
   return (
     <div className="content">
       <h1>Rsbuild with React</h1>
+
       <p>Start building asd things with Rsbuild.</p>
       <Button onClick={handleClick}>Clique</Button>
     </div>
